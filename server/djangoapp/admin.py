5739c488635d2cec2dd59c44ae4795c132e90e24
gulcantpl <gulcan.tpl9049@gmail.com>
2025-09-23 14:01:25 -0400

    car make model added

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,23 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
-
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1
 # CarModelAdmin class
-
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    inlines = [CarModelInline]
 # CarMakeAdmin class with CarModelInline
-
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')
+    list_filter = ('car_make', 'type', 'year')
+    search_fields = ('name', 'car_make__name')
 # Register models here
+admin.site.register(CarMake)
+admin.site.register(CarModel)
